63d3dbedf005bf859edfc40e2b926b02338572ed
Joseph Banks <joseph@josephbanks.me>
2018-09-24 19:19:10 +0100

    Make the command line runner

--- README.md
+++ README.md
@@ -1,1 +1,10 @@
 # Sef interpreter
+
+## Example code
+
+Requires Python 3.7 because I use dataclasses.
+
+```bash
+$ python3.7 -m sef example.sef
+...
+```
--- sef/__main__.py
+++ sef/__main__.py
@@ -2,8 +2,25 @@
 The Sef interpreter.
 """
 
-import re
+import sys
 
 from sef.token import Tokenizer
+from sef.parser import Parser
+from sef.logic import Interpreter
 
 __author__ = "Joseph Banks"
+
+if __name__ == "__main__":
+    path = sys.argv[1]
+
+    with open(path, "r") as f:
+        contents = f.read()
+
+    tokenizer = Tokenizer(contents)
+    tokens = tokenizer.tokenize_code()
+
+    parser = Parser(tokens)
+    ast = parser.parse()
+
+    interpreter = Interpreter(ast)
+    interpreter.begin_execution()
